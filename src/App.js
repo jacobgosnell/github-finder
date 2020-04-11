aa33cc82f1c8dfd1e243ad4dfcd94a10facb2f8b
Jake Gosnell <jake@hangar31development.com>
2020-04-11 13:56:37 -0700

    fetched data from github api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import './App.css';
 
 function App() {
-  return (
-		<div className="App">
+   const [ users, setUsers ] = useState([]);
+   const [ loading, isLoading ] = useState(false);
+   useEffect(() => {
+      // promises syntax
+      //  axios.get('https://api.github.com/users').then(res => console.log(res.data))
+      // async await syntax
+      async function fetchData() {
+         isLoading(true);
+         const res = await axios.get('https://api.github.com/users');
+         setUsers(res.data);
+      }
+      fetchData();
+      isLoading(false);
+   }, [])
+
+   return (
+     	<div className="App">
          <Navbar />
          <div className="container">
-            <Users />
+            <Users loading={loading} users={users} />
          </div>
 		</div>
 	);
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,8 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 class Users extends Component {
-	state = {
-		users: [
-			{
-				id: "1",
-				login: "mojombo",
-				avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-				html_url: "https://github.com/mojombo",
-			},
-			{
-				id: "2",
-				login: "defunkt",
-				avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-				html_url: "https://github.com/defunkt",
-			},
-			,
-			{
-				id: "3",
-				login: "pjhyett",
-				avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-				html_url: "https://github.com/pjhyett",
-			},
-		],
-	};
-
 	render() {
-    const { users } = this.state;
+    const { users } = this.props;
 		return (
 			<div style={userStyle}>
 				{users.map(user => (
